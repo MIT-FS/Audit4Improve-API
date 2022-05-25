6a371f7860f3e950d4f30eec69be95b19b7135ac
Jia-Lin Shi <shi.jialin98@gmail.com>
2022-05-25 17:39:04 +0200

    added javadoc, added setter

--- src/main/java/us/muit/fs/a4i/config/MyFont.java
+++ src/main/java/us/muit/fs/a4i/config/MyFont.java
@@ -10,19 +10,34 @@ public class MyFont extends Font{
 	 */
 	private static final long serialVersionUID = 1L;
 
+	// Color of the font
 	private Color color;
 	
-	protected MyFont(Font font) {
-		super(font);
-	}
-	
+	/**
+	 * Constructor of the MyFont class. Calls the constructor of
+	 * the Font class and sets the color.
+	 * 
+	 * @param font the font to be set
+	 * @param color the color to be set
+	 */
 	protected MyFont(Font font, Color color) {
 		super(font);
 		this.color = color;
 	}
 	
+	/**
+	 * Getter for color
+	 * @return the color
+	 */
 	public Color getColor() {
 		return color;
 	}
-
+	
+	/**
+	 * Setter for color
+	 * @param color the color to be set/changed
+	 */
+	public void setColor(Color color) {
+		this.color = color;
+	}
 }
