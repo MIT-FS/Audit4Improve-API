cdf2e543dfc02fad18239de6647e48f0581388de
javaldher <javieraldanahernandez@gmail.com>
2025-05-24 18:15:03 +0200

    IndicatorStrategy hecho

--- src/test/java/us/muit/fs/a4i/test/control/strategies/TARIndicatorStrategyTest.java
+++ src/test/java/us/muit/fs/a4i/test/control/strategies/TARIndicatorStrategyTest.java
@@ -9,137 +9,95 @@ import java.util.logging.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import us.muit.fs.a4i.control.strategies.IEFStrategy;
+import us.muit.fs.a4i.control.strategies.TARIndicatorStrategy;
 import us.muit.fs.a4i.exceptions.NotAvailableMetricException;
 import us.muit.fs.a4i.exceptions.ReportItemException;
 import us.muit.fs.a4i.model.entities.IndicatorI.IndicatorState;
 import us.muit.fs.a4i.model.entities.ReportItemI;
 import us.muit.fs.a4i.model.entities.ReportItem;
 
-public class IEFStrategyTest {
-    private static Logger log = Logger.getLogger(IEFStrategyTest.class.getName());
-    private IEFStrategy strat;
+public class TARIndicatorStrategyTest {
+    private static Logger log = Logger.getLogger(TARIndicatorStrategyTest.class.getName());
+    private TARIndicatorStrategy strat;
 
     @BeforeEach
     void setUp() {
-        strat = new IEFStrategy();
+        strat = new TARIndicatorStrategy();
     }
+
     @Test
-    public void testIEF_Excelente() throws NotAvailableMetricException, ReportItemException {
-        // Creamos mocks para cada métrica
-        ReportItemI<Double> mC = mock(ReportItemI.class);
-        ReportItemI<Double> mW = mock(ReportItemI.class);
-        ReportItemI<Double> mT = mock(ReportItemI.class);
-        ReportItemI<Double> mP = mock(ReportItemI.class);
+    public void testTAR_Alto() throws NotAvailableMetricException, ReportItemException {
+        ReportItemI<Double> visitors = mock(ReportItemI.class);
+        ReportItemI<Double> clones = mock(ReportItemI.class);
 
-        when(mC.getName()).thenReturn("cycleTime");
-        when(mC.getValue()).thenReturn(0.0);
-        when(mW.getName()).thenReturn("waitTime");
-        when(mW.getValue()).thenReturn(0.0);
-        when(mT.getName()).thenReturn("throughput");
-        when(mT.getValue()).thenReturn(50.0);
-        when(mP.getName()).thenReturn("WIP");
-        when(mP.getValue()).thenReturn(0.0);
+        when(visitors.getName()).thenReturn("uniqueVisitors");
+        when(visitors.getValue()).thenReturn(60.0);
+        when(clones.getName()).thenReturn("uniqueClones");
+        when(clones.getValue()).thenReturn(30.0);
 
-        List<ReportItemI<Double>> metrics = List.of(mC, mW, mT, mP);
-        log.info("Métricas de entrada: " + metrics);
-
+        List<ReportItemI<Double>> metrics = List.of(visitors, clones);
         ReportItemI<Double> result = strat.calcIndicator(metrics);
-        log.info("Resultado IEF: " + result);
 
-        assertEquals("IEF", result.getName());
-        assertEquals(1.0, result.getValue(), 1e-6);
-        assertNotNull(result.getIndicator());
+        assertEquals("GithubTraffic", result.getName());
+        assertEquals(0.5, result.getValue(), 1e-6);
         assertEquals(IndicatorState.OK, result.getIndicator().getState());
     }
 
     @Test
-    public void testIEF_Bueno() throws NotAvailableMetricException, ReportItemException {
-        ReportItemI<Double> mC = mock(ReportItemI.class);
-        ReportItemI<Double> mW = mock(ReportItemI.class);
-        ReportItemI<Double> mT = mock(ReportItemI.class);
-        ReportItemI<Double> mP = mock(ReportItemI.class);
+    public void testTAR_Medio() throws NotAvailableMetricException, ReportItemException {
+        ReportItemI<Double> visitors = mock(ReportItemI.class);
+        ReportItemI<Double> clones = mock(ReportItemI.class);
 
-        when(mC.getName()).thenReturn("cycleTime");
-        when(mC.getValue()).thenReturn(40.0);  // 0.25
-        when(mW.getName()).thenReturn("waitTime");
-        when(mW.getValue()).thenReturn(20.0);  // 0.25
-        when(mT.getName()).thenReturn("throughput");
-        when(mT.getValue()).thenReturn(35.0);  // 0.7
-        when(mP.getName()).thenReturn("WIP");
-        when(mP.getValue()).thenReturn(5.0);   // 0.25
+        when(visitors.getName()).thenReturn("uniqueVisitors");
+        when(visitors.getValue()).thenReturn(30.0);
+        when(clones.getName()).thenReturn("uniqueClones");
+        when(clones.getValue()).thenReturn(9.0);
 
-        List<ReportItemI<Double>> metrics = List.of(mC, mW, mT, mP);
+        List<ReportItemI<Double>> metrics = List.of(visitors, clones);
         ReportItemI<Double> result = strat.calcIndicator(metrics);
 
-        assertEquals("IEF", result.getName());
-        assertEquals(0.8125, result.getValue(), 1e-4);  // cae en "Excelente"
-        assertNotNull(result.getIndicator());
-        assertEquals(IndicatorState.OK, result.getIndicator().getState());  // este valor da "Excelente"
-    }
-
-    @Test
-    public void testIEF_Aceptable() throws NotAvailableMetricException, ReportItemException {
-        // Creamos mocks para cada métrica
-        ReportItemI<Double> mC = mock(ReportItemI.class);
-        ReportItemI<Double> mW = mock(ReportItemI.class);
-        ReportItemI<Double> mT = mock(ReportItemI.class);
-        ReportItemI<Double> mP = mock(ReportItemI.class);
-
-        when(mC.getName()).thenReturn("cycleTime");
-        when(mC.getValue()).thenReturn(80.0);
-        when(mW.getName()).thenReturn("waitTime");
-        when(mW.getValue()).thenReturn(40.0);
-        when(mT.getName()).thenReturn("throughput");
-        when(mT.getValue()).thenReturn(25.0);
-        when(mP.getName()).thenReturn("WIP");
-        when(mP.getValue()).thenReturn(10.0);
-
-        List<ReportItemI<Double>> metrics = List.of(mC, mW, mT, mP);
-        log.info("Métricas de entrada: " + metrics);
-
-        ReportItemI<Double> result = strat.calcIndicator(metrics);
-        log.info("Resultado IEF: " + result);
-
-        assertEquals("IEF", result.getName());
-        assertEquals(0.5, result.getValue(), 1e-6);
-        assertNotNull(result.getIndicator());
+        assertEquals("GithubTraffic", result.getName());
+        assertEquals(0.3, result.getValue(), 1e-6);
         assertEquals(IndicatorState.WARNING, result.getIndicator().getState());
     }
 
     @Test
-    public void testIEF_Bajo() throws NotAvailableMetricException, ReportItemException {
-        // Creamos mocks para cada métrica
-        ReportItemI<Double> mC = mock(ReportItemI.class);
-        ReportItemI<Double> mW = mock(ReportItemI.class);
-        ReportItemI<Double> mT = mock(ReportItemI.class);
-        ReportItemI<Double> mP = mock(ReportItemI.class);
+    public void testTAR_Bajo() throws NotAvailableMetricException, ReportItemException {
+        ReportItemI<Double> visitors = mock(ReportItemI.class);
+        ReportItemI<Double> clones = mock(ReportItemI.class);
 
-        when(mC.getName()).thenReturn("cycleTime");
-        when(mC.getValue()).thenReturn(200.0);
-        when(mW.getName()).thenReturn("waitTime");
-        when(mW.getValue()).thenReturn(100.0);
-        when(mT.getName()).thenReturn("throughput");
-        when(mT.getValue()).thenReturn(0.0);
-        when(mP.getName()).thenReturn("WIP");
-        when(mP.getValue()).thenReturn(30.0);
+        when(visitors.getName()).thenReturn("uniqueVisitors");
+        when(visitors.getValue()).thenReturn(5.0);
+        when(clones.getName()).thenReturn("uniqueClones");
+        when(clones.getValue()).thenReturn(1.0);
 
-        List<ReportItemI<Double>> metrics = List.of(mC, mW, mT, mP);
-        log.info("Métricas de entrada: " + metrics);
-
+        List<ReportItemI<Double>> metrics = List.of(visitors, clones);
         ReportItemI<Double> result = strat.calcIndicator(metrics);
-        log.info("Resultado IEF: " + result);
 
-        assertEquals("IEF", result.getName());
-        assertEquals(0.0, result.getValue(), 1e-6);
-        assertNotNull(result.getIndicator());
+        assertEquals("GithubTraffic", result.getName());
+        assertEquals(0.2, result.getValue(), 1e-6);
         assertEquals(IndicatorState.CRITICAL, result.getIndicator().getState());
     }
 
     @Test
-    public void testIEF_FaltanMetricas() throws ReportItemException {
-        ReportItemI<Double> mC = new ReportItem.ReportItemBuilder<>("cycleTime", 10.0).build();
-        List<ReportItemI<Double>> metrics = List.of(mC);
+    public void testTAR_VisitantesCero() {
+        ReportItemI<Double> visitors = mock(ReportItemI.class);
+        ReportItemI<Double> clones = mock(ReportItemI.class);
+
+        when(visitors.getName()).thenReturn("uniqueVisitors");
+        when(visitors.getValue()).thenReturn(0.0);
+        when(clones.getName()).thenReturn("uniqueClones");
+        when(clones.getValue()).thenReturn(5.0);
+
+        List<ReportItemI<Double>> metrics = List.of(visitors, clones);
+
+        assertThrows(NotAvailableMetricException.class, () -> strat.calcIndicator(metrics));
+    }
+
+    @Test
+    public void testTAR_FaltanMetricas() throws ReportItemException {
+        ReportItemI<Double> onlyVisitors = new ReportItem.ReportItemBuilder<>("uniqueVisitors", 25.0).build();
+        List<ReportItemI<Double>> metrics = List.of(onlyVisitors);
 
         assertThrows(NotAvailableMetricException.class, () -> strat.calcIndicator(metrics));
     }
